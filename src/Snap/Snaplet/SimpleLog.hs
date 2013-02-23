75db9f431891a6683984f98b95e7177dd88f992b
Dmitry Dzhus <dima@dzhus.org>
2013-02-23 04:41:13 +0400

    Bump snap dep to 0.11

--- src/Snap/Snaplet/SimpleLog.hs
+++ src/Snap/Snaplet/SimpleLog.hs
@@ -12,7 +12,6 @@ module Snap.Snaplet.SimpleLog (
 import Control.Monad
 import Control.Monad.State
 import Control.Monad.IO.Class
-import Data.Lens.Template
 import Snap.Core
 import Snap.Snaplet
 import System.Log.Simple
